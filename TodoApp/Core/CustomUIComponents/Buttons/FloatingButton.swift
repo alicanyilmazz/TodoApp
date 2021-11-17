553992e6880d8e0c195e5f25b6a086f272c1fb10
alicanyilmazz <alicanyilmaz101@gmail.com>
2021-11-17 22:05:27 +0300

    Custom Floating Button was added.

--- TodoApp/Core/CustomUIComponents/Buttons/FloatingButton.swift
+++ TodoApp/Core/CustomUIComponents/Buttons/FloatingButton.swift
@@ -10,13 +10,27 @@ struct FloatingButtonViewModel{
     let shadowOpacity : Float
     let cornerRadius : CGFloat
     
-    static let add : FloatingButtonViewModel = FloatingButtonViewModel(image: UIImage(systemName: "plus",withConfiguration: UIImage.SymbolConfiguration(pointSize: 32,weight: .medium)), tintColor: .white, setTitleColor: .white, backgroundColor: .systemPink, shadowRadius: 10, shadowOpacity: 0.3, cornerRadius: 30)
+    static let add : FloatingButtonViewModel = FloatingButtonViewModel(image: UIImage(systemName: ImageName.plus.description,withConfiguration: UIImage.SymbolConfiguration(pointSize: 32,weight: .medium)), tintColor: .white, setTitleColor: .white, backgroundColor: ColorPalette.darkpink, shadowRadius: 10, shadowOpacity: 0.3, cornerRadius: 30)
     
-    static let settings : FloatingButtonViewModel = FloatingButtonViewModel(image: UIImage(systemName: "moon",withConfiguration: UIImage.SymbolConfiguration(pointSize: 20,weight: .medium)), tintColor: .white, setTitleColor: .white, backgroundColor: .systemPink, shadowRadius: 10, shadowOpacity: 0.3, cornerRadius: 22)
+    static let settings : FloatingButtonViewModel = FloatingButtonViewModel(image: UIImage(systemName: ImageName.moon.description,withConfiguration: UIImage.SymbolConfiguration(pointSize: 20,weight: .medium)), tintColor: .white, setTitleColor: .white, backgroundColor: ColorPalette.darkpink, shadowRadius: 10, shadowOpacity: 0.3, cornerRadius: 22)
+}
+
+struct CustomFloatingButtonViewModel{
+    let image : UIImage?
+    let setTitleColor : UIColor?
+    let shadowRadius : CGFloat
+    let shadowOpacity : Float
+    let cornerRadius : CGFloat
+    
+    static let add : CustomFloatingButtonViewModel = CustomFloatingButtonViewModel(image: UIImage(systemName: ImageName.plus.description,withConfiguration: UIImage.SymbolConfiguration(pointSize: 32,weight: .medium)), setTitleColor: .white, shadowRadius: 10, shadowOpacity: 0.3, cornerRadius: 30)
+    
+    static let settings : CustomFloatingButtonViewModel = CustomFloatingButtonViewModel(image: UIImage(systemName: ImageName.moon.description,withConfiguration: UIImage.SymbolConfiguration(pointSize: 20,weight: .medium)), setTitleColor:.white,
+        shadowRadius: 10, shadowOpacity: 0.3, cornerRadius: 22)
 }
 
 final class FloatingButton : UIButton{
-    let image = UIImage(systemName: "plus",withConfiguration: UIImage.SymbolConfiguration(pointSize: 32,weight: .medium))
+    let image = UIImage(systemName: ImageName.plus.description,withConfiguration: UIImage.SymbolConfiguration(pointSize: 32,weight: .medium))
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .systemPink
@@ -26,7 +40,6 @@ final class FloatingButton : UIButton{
         layer.shadowRadius = 10
         layer.shadowOpacity = 0.3
         layer.cornerRadius = 30
-        
     }
     
     required init?(coder: NSCoder) {
@@ -43,9 +56,29 @@ final class FloatingButton : UIButton{
         layer.cornerRadius = viewModel.cornerRadius
     }
     
+    func customConfigure(with viewModel : CustomFloatingButtonViewModel){
+        theme.backgroundColor = themed { $0.addButtonBackgroundColor }
+        setImage(viewModel.image, for: .normal)
+        theme.tintColor = themed { $0.addButtonTintColor }
+        setTitleColor(viewModel.setTitleColor, for: .normal)
+        layer.shadowRadius = viewModel.shadowRadius
+        layer.shadowOpacity = viewModel.shadowOpacity
+        layer.cornerRadius = viewModel.cornerRadius
+    }
+        
     override func layoutSubviews() {
         super.layoutSubviews()
     }
 }
 
-//CGRect(x: view.frame.size.width - 60 - 18, y: view.frame.size.height - 70 - 8 - view.safeAreaInsets.bottom, width: 60, height: 60)
+
+func setLigthModeIcon(_ themeButton : FloatingButton){
+        themeButton.setImage(UIImage(systemName: ImageName.moon.description), for: .normal)
+    }
+    
+func setDarkModeIcon(_ themeButton : FloatingButton){
+        themeButton.setImage(UIImage(systemName: ImageName.sun.description), for: .normal)
+    }
+
+
+
