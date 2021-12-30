6bf4f6af78a6efd64be390d2a512d18855167ab4
alicanyilmazz <alicanyilmaz101@gmail.com>
2021-12-31 01:32:42 +0300

    TodoDetailPresentation was refactored for isTheNotificationScheduled.

--- TodoApp/Presentations/TodoDetailPresentation.swift
+++ TodoApp/Presentations/TodoDetailPresentation.swift
@@ -12,23 +12,27 @@ final class TodoDetailPresentation: NSObject {
     let explanation : String
     let date : Date
     let isCompleted : Bool
+    let notificationId : String
+    let isTheNotificationScheduled : Bool
     
-    init(detailTitle: String,explanation : String ,date : Date ,isCompleted : Bool ){
+    init(detailTitle: String,explanation : String ,date : Date ,isCompleted : Bool , notificationId : String , isTheNotificationScheduled : Bool){
         self.detailTitle = detailTitle
         self.explanation = explanation
         self.date = date
         self.isCompleted = isCompleted
+        self.notificationId = notificationId
+        self.isTheNotificationScheduled = isTheNotificationScheduled
         super.init()
     }
     
     override func isEqual(_ object: Any?) -> Bool {
         guard let other = object as? TodoDetailPresentation else { return false }
-        return self.detailTitle == other.detailTitle && self.explanation == other.explanation && self.date == other.date && self.isCompleted == other.isCompleted
+        return self.detailTitle == other.detailTitle && self.explanation == other.explanation && self.date == other.date && self.isCompleted == other.isCompleted && self.notificationId == other.notificationId && self.isTheNotificationScheduled == isTheNotificationScheduled
     }
 }
 
 extension TodoDetailPresentation{
     convenience init(todoDetail : TodoDetail){
-        self.init(detailTitle: todoDetail.title!, explanation: todoDetail.explation!, date: todoDetail.date!, isCompleted: todoDetail.isCompleted)
+        self.init(detailTitle: todoDetail.title!, explanation: todoDetail.explation!, date: todoDetail.date!, isCompleted: todoDetail.isCompleted,notificationId: todoDetail.notificationId!,isTheNotificationScheduled: todoDetail.isTheNotificationScheduled)
     }
 }
