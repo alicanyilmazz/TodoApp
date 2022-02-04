5736634fc7302c6d5c098c015b832e2cddf09bb1
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-02-04 23:32:45 +0300

    Image function added to CustomImage.

--- TodoApp/Core/CustomUIComponents/Images/UIImageExtensions.swift
+++ TodoApp/Core/CustomUIComponents/Images/UIImageExtensions.swift
@@ -33,4 +33,20 @@ struct CustomImage{
     static let checkmark = (UIImage(systemName: ImageName.bell.description)?.withTintColor(ColorPalette.yellow ,renderingMode: .alwaysOriginal))!
     
     static let circle = (UIImage(systemName: ImageName.circle.description)?.withTintColor(UIColor.getRandomColor() ,renderingMode: .alwaysOriginal))!
+    
+    static func Image(tag : Int) -> UIImage{
+        let image : UIImage!
+        switch tag{
+        case 0 : image = UIImage(systemName: ImageName.circle.description)?.withTintColor(ColorPalette.iconBlue,renderingMode: .alwaysOriginal)
+        case 1 : image = UIImage(systemName: ImageName.circle.description)?.withTintColor(ColorPalette.iconRed,renderingMode: .alwaysOriginal)
+        case 2 : image = UIImage(systemName: ImageName.circle.description)?.withTintColor(ColorPalette.iconGray,renderingMode: .alwaysOriginal)
+        case 3 : image = UIImage(systemName: ImageName.circle.description)?.withTintColor(ColorPalette.iconGreen,renderingMode: .alwaysOriginal)
+        case 4 : image = UIImage(systemName: ImageName.circle.description)?.withTintColor(ColorPalette.iconOrange,renderingMode: .alwaysOriginal)
+        case 5 : image = UIImage(systemName: ImageName.circle.description)?.withTintColor(ColorPalette.iconPurple,renderingMode: .alwaysOriginal)
+        default: image = UIImage(systemName: ImageName.circle.description)?.withTintColor(ColorPalette.iconBlue,renderingMode: .alwaysOriginal)
+        }
+        return image
+    }
 }
+
+
