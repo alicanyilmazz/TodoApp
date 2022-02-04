a548ea1542f43b2fda4c64d387e6b0dfe1186b6b
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-02-04 23:59:08 +0300

    New fields added to TodoPresentation.

--- TodoApp/Presentations/TodoPresentation.swift
+++ TodoApp/Presentations/TodoPresentation.swift
@@ -10,20 +10,24 @@ import Foundation
 final class TodoPresentation: NSObject {
     
     let title: String
+    let colorType : Int
+    let id : String
     
-    init(title: String) {
+    init(title: String , colorType : Int , id : String) {
         self.title = title
+        self.colorType = colorType
+        self.id = id
         super.init()
     }
     
     override func isEqual(_ object: Any?) -> Bool {
         guard let other = object as? TodoPresentation else { return false }
-        return self.title == other.title
+        return self.title == other.title && self.colorType == other.colorType && self.id == other.id
     }
 }
 
 extension TodoPresentation {
     convenience init(todo: Todo) {
-        self.init(title: todo.title!)
+        self.init(title: todo.title!,colorType: Int(todo.colorType),id: todo.id!)
     }
 }
