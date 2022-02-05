b510a33868a1b8a067f6d4a1ecdc5a16764d7fb4
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-02-05 15:29:34 +0300

    insertData added and addTodoDetail removed.

--- TodoApp/Scenes/TodoExplanation/TodoExplanationViewModel.swift
+++ TodoApp/Scenes/TodoExplanation/TodoExplanationViewModel.swift
@@ -12,30 +12,44 @@ final class TodoExplanationViewModel: TodoExplanationViewModelProtocol {
       
     weak var delegate: TodoExplanationViewModelDelegate?
     private let service : TodoDetailListServiceProtocol
-    private let presentation: TodoExplanationPresentation
-    private var todoDetail: TodoDetail = TodoDetail()
-    private var todo : Todo = Todo()
-    private let index : Int
+    private var presentation: TodoExplanationPresentation?
+    private var todoDetail: TodoDetail?
+    private var todoId : String?
+    private var todoDetailId : String?
+    private let operationType : operationTypes?
 
     
-    init(service : TodoDetailListServiceProtocol , todoDetail : TodoDetail, todo : Todo , index : Int) {
+    init(service : TodoDetailListServiceProtocol , todoDetailId : String, todoId : String , operationType : operationTypes) {
         self.service = service
-        self.todoDetail = todoDetail
-        self.index = index
-        self.todo = todo
-        self.presentation = TodoExplanationPresentation(todoDetail: self.todoDetail)
+        self.todoId = todoId
+        self.todoDetailId = todoDetailId
+        self.operationType = operationType
     }
     
     func load() {
-        delegate?.showDetail(presentation)
+        if operationType == .edit{
+            todoDetail = service.fetchTodoDetail(with: TodoDetail.fetchRequest(),todoDetailId: todoDetailId!, predicate: nil)
+            self.presentation = TodoExplanationPresentation(todoDetail: todoDetail!)
+            delegate?.showDetail(presentation!)
+        }else if operationType == .add{
+            self.presentation = TodoExplanationPresentation(detailTitle: "", explanation: "", date: Date(), isCompleted: true, notificationId: UUID().uuidString, isTheNotificationScheduled: false, colorType: 0, id: UUID().uuidString)
+            delegate?.showDetail(presentation!)
+        }
     }
     
-    func addTodoDetail(title : String , explanation : String , date : Date , iscCompleted : Bool , isTheNotificationScheduled : Bool) {
-        todoDetail.title = title
-        todoDetail.explation = explanation
-        todoDetail.date = date
-        todoDetail.isCompleted = iscCompleted
-        todoDetail.isTheNotificationScheduled = isTheNotificationScheduled
-        service.addTodoDetail(todoDetail: todoDetail, todo: todo)
+    func insertData(title : String , explanation : String , date : Date , iscCompleted : Bool , isTheNotificationScheduled : Bool , colorType : Int) {
+        if operationType == .edit{
+            todoDetail?.title = title
+            todoDetail?.explation = explanation
+            todoDetail?.date = date
+            todoDetail?.isCompleted = iscCompleted
+            todoDetail?.isTheNotificationScheduled = isTheNotificationScheduled
+            todoDetail?.colorType = Int16(colorType)
+            todoDetail?.notificationId = presentation?.notificationId
+            todoDetail?.id = presentation?.id
+            service.editTodoDetail(todoDetail: todoDetail!)
+        }else if operationType == .add{
+            service.addTodoDetail(title: title, explanation: explanation, date: date, iscCompleted: iscCompleted, isTheNotificationScheduled: isTheNotificationScheduled, colorType: colorType, todoId: todoId!, notificationId: self.presentation!.notificationId, id: self.presentation!.id)
+        }
     }
 }
