3a3f71fc63a5563bd1b0fde67622c3044f85d2d7
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-02-05 15:50:15 +0300

    TodoListAlertRouter added.

---
+++ TodoApp/Scenes/TodoListAlert/TodoListAlertRouter.swift
@@ -0,0 +1,29 @@
+//
+//  TodoListAlertRouter.swift
+//  TodoApp
+//
+//  Created by alican on 12.01.2022.
+//
+
+import Foundation
+/*
+final class TodoListAlertRouter : TodoListAlertRouterProtocol{
+
+     unowned let view: UIViewController
+     var service : TodoListAlertServiceProtocol!
+     
+     init(view: UIViewController) {
+         self.view = view
+     }
+
+    func navigate(to route: TodoListAlertRoute) {
+        switch route {
+        case .unknown(let todo):
+            service = TodoListService()
+            let unknownListViewModel = TodoUnknownViewModel(service: service, todo: todo)
+            let unknownView = TodoDetailBuilder.make(with: unknownListViewModel, todo: todo)
+            view.show(unknownView, sender: nil)
+        }
+    }
+}
+*/
